745b157c8dca6f208adfc789881467f7c9a6de3d
xposed73 <46350409+xposed73@users.noreply.github.com>
2026-04-23 14:28:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,19 @@ echo "🚀 Starting Radio System..."
 
 systemctl start icecast2
 
-# run generator once
+# update delay mapping
 bash /root/generate_delays.sh
 
-# start liquidsoap
+# kill old
 pkill liquidsoap 2>/dev/null || true
+sleep 2
+
+# start liquidsoap
 nohup liquidsoap /root/delay_48.liq > /root/liquid.log 2>&1 &
+
+sleep 2
+
+ps aux | grep liquidsoap | grep -v grep
 
 echo "✅ System Started"
 echo "🌐 http://$(curl -s ifconfig.me):8000"
