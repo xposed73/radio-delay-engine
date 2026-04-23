c564481acf710e10fe6a797cc0640c47ee9d8250
xposed73 <46350409+xposed73@users.noreply.github.com>
2026-04-23 15:11:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,20 @@
 
 echo "🚀 Starting system..."
 
-systemctl start icecast2
+systemctl restart icecast2
 
 bash /root/generate_delays.sh
 
 pkill liquidsoap 2>/dev/null || true
 sleep 2
 
+ulimit -n 100000
+
 nohup liquidsoap /root/delay_48.liq > /root/liquid.log 2>&1 &
+
+sleep 2
+
+ps aux | grep liquidsoap | grep -v grep
 
 echo "✅ Running"
 echo "🌐 http://$(curl -s ifconfig.me):8000"
