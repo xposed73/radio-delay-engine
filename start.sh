121aac8f6bbe1adbc18e11082b5673b7dfa02c9f
xposed73 <46350409+xposed73@users.noreply.github.com>
2026-04-23 15:28:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,10 @@ bash /root/generate_delays.sh
 pkill liquidsoap 2>/dev/null || true
 sleep 2
 
-ulimit -n 100000
+# recording process
+nohup liquidsoap /root/record.liq > /root/record.log 2>&1 &
 
+# streaming process
 nohup liquidsoap /root/delay_48.liq > /root/liquid.log 2>&1 &
 
 sleep 2
